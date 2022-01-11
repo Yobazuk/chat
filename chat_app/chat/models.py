69fdb544a3f531f746d2fb0704352dd487046383
yobaz <yobazuk@gmail.com>
2022-01-11 20:19:51 +0200

    Added ChatMessage and InfoMessage models for future improvements.

--- chat_app/chat/models.py
+++ chat_app/chat/models.py
@@ -8,3 +8,13 @@ class Message(models.Model):
 
     class Meta:
         ordering = ('date_added',)
+
+
+class ChatMessage(Message):
+    def get_html(self):
+        return f"<b>{self.username}</b>: {self.content}<br>"
+
+
+class InfoMessage(Message):
+    def get_html(self):
+        return f'<center><small class="has-text-grey-light">{self.content}</small></center>'
